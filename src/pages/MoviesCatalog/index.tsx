b9fed35f04f1490c92fda9c03a465eec36196ce9
juliodumont <julioef.dumont@gmail.com>
2022-08-19 07:32:48 -0300

    Pagination done, still working on genre selector

--- src/pages/MoviesCatalog/index.tsx
+++ src/pages/MoviesCatalog/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GenreSelector from "../../components/GenreSelector";
 import MovieCard from "../../components/MovieCard";
+import Pagination from "../../components/Pagination";
 import { MovieInformation } from "../../types/movie";
 import { Page } from "../../types/vendor/page";
 import { requestBackend } from "../../util/requests";
@@ -12,45 +13,48 @@ function MoviesCatalog() {
   const [movieInformation, setMovieInformation] =
     useState<Page<MovieInformation>>();
 
-  const movieTest: MovieInformation = {
-    id: 6,
-    title: "A Voz do Silêncio",
-    subTitle: "Koe no Katachi",
-    year: 2016,
-    imgUrl:
-      "https://image.tmdb.org/t/p/w533_and_h300_bestv2/5lAMQMWpXMsirvtLLvW7cJgEPkU.jpg",
-  };
-
-  useEffect(() => {
+  const getMovies = (pageNumber: number) => {
     const params: AxiosRequestConfig = {
       url: "/movies",
       withCredentials: true,
       params: {
-        page: 0,
+        page: pageNumber,
         size: 8,
       },
     };
     requestBackend(params).then((response) => {
       setMovieInformation(response.data);
     });
+  }
+
+  const handleGenreSelect = () =>{
+    
+  }
+
+  useEffect(() => {
+    getMovies(0)
   }, []);
 
   return (
     <main className="movie-catalog-container">
       <div>
-        <GenreSelector onGenreSelect={() => {}}/>
+        <GenreSelector onGenreSelect={handleGenreSelect} />
       </div>
       <div className="catalog-container">
         {movieInformation?.content.map((movie) => (
           <div className="catalog-movie-container" key={movie.id}>
-            <Link
-              to={`/movies/${movie.id}`}
-              className={"movie-details-link"}
-            >
+            <Link to={`/movies/${movie.id}`} className={"movie-details-link"}>
               <MovieCard size={"sm"} movie={movie} showDescription={false} />
             </Link>
           </div>
         ))}
+      </div>
+      <div>
+        <Pagination
+          pageCount={movieInformation ? movieInformation.totalPages : 0}
+          range={3}
+          onChange={getMovies}
+        />
       </div>
     </main>
   );
